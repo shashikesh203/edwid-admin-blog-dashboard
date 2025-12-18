682e5c15eb5461d1d4714d88280922eb8408e4e6
Shashikesh kumar <uitshashikesh@gmail.com>
2025-12-18 20:17:12 +0530

    fixed style issue

--- src/components/GenericInput/SearchBox.tsx
+++ src/components/GenericInput/SearchBox.tsx
@@ -1,7 +1,7 @@
 export default function SearchBox({ searchBlogPosts }: { searchBlogPosts: (query: string) => void }) {
     return (
-        <div className="flex items-center gap-2 sm:gap-4 flex-1 justify-end py-4">
-            <div className="relative max-w-md flex-1  sm:block">
+        <div className="flex items-center gap-2 sm:gap-4 flex-1 justify-end md:py-4 w-full">
+            <div className="relative  w-full md:max-w-md flex-1  sm:block">
                 <input
                     type="text"
                     onChange={(e) => searchBlogPosts(e.target.value)}
--- src/pages/Blogs.tsx
+++ src/pages/Blogs.tsx
@@ -26,7 +26,7 @@ export default function Blogs() {
 
   const [currentPage, setCurrentPage] = useState(1);
   const [totalItems, setTotalItems] = useState(0);
-  const PAGE_SIZE = 1;
+  const PAGE_SIZE = 5;
 
   const getFiltered = (
     source: BlogPost[] = allBlogPosts,
@@ -100,10 +100,10 @@ export default function Blogs() {
   const totalPages = Math.max(1, Math.ceil(totalItems / PAGE_SIZE));
 
   return (
-    <div className="relative min-h-[60vh]">
-      <div className="flex flex-col sm:flex-row items-center gap-4 sm:gap-6 justify-between px-4 sm:px-6 lg:px-8 py-6">
-        <div className="w-full sm:w-auto flex items-center gap-3">
-          <div className="min-w-48 w-48">
+    <div className="relative min-h-screen pb-8">
+      <div className="flex flex-col sm:flex-row sm:item items-center gap-4 sm:gap-6 justify-between px-4 sm:px-6 lg:px-8 py-6">
+        <div className="w-full sm:w-auto flex sm:items-center gap-3 sm:justify-center">
+          <div className="w-full md:min-w-48 md:w-48">
             <SelectOptions
               name="categoryFilter"
               value={selectedCategory}
@@ -121,13 +121,13 @@ export default function Blogs() {
 
         <button
           onClick={() => setIsAddOpen(true)}
-          className="flex items-center gap-2 px-3 sm:px-6 py-2.5 bg-blue-900 text-white rounded-lg hover:bg-blue-800 transition-colors font-medium shadow-sm hover:shadow-md hover:cursor-pointer flex-shrink-0"
+          className="w-full md:w-50 flex items-center  justify-center gap-2 px-3 sm:px-6 py-2.5 bg-blue-900 text-white rounded-lg hover:bg-blue-800 transition-colors font-medium shadow-sm hover:shadow-md hover:cursor-pointer flex-shrink-0"
         >
           <svg className="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24">
             <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M12 4v16m8-8H4" />
           </svg>
-          <span className="hidden sm:inline">Add New Blog</span>
-          <span className="sm:hidden">Add</span>
+          <span className="inlin ">Add New Blog</span>
+
         </button>
       </div>
 
